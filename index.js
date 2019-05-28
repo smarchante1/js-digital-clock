b4abbdd9371d77c630b60b2d77ced6fded143858
Stephanie Marchante <smarcha04@gmail.com>
2019-05-28 14:00:24 -0700

    adding color and alignment buttons and weird bootstrap thing

--- index.js
+++ index.js
@@ -33,7 +33,52 @@ const clock = function clock(){
   document.getElementById("ClockTower").innerText = time;
     
   setTimeout(clock, 1000);
- 
-}
+
+  // const COLORS = ['blue', 'purple', 'yellow'];
+  // let colorIndex = 0;
+  
+  // const nextColor = function nextColor() {
+  //   let color = COLORS[colorIndex];
+  //   colorIndex += 1;
+  //   colorIndex %= COLORS.length;
+  //   return color;
+  // };
+
+  $('#purple-button').on('click', 'button', function(event) {
+    let target = $('#ClockTower');
+      target.removeClass().addClass('purple');
+  });
+
+  $('#blue-button').on('click', 'button', function(event) {
+    let target = $('#ClockTower');
+      target.removeClass().addClass('blue');
+  });
+
+  $('#yellow-button').on('click', 'button', function(event) {
+    let target = $('#ClockTower');
+      target.removeClass().addClass('yellow');
+  });
+
+  $('#pink-button').on('click', 'button', function(event) {
+    let target = $('#ClockTower');
+      target.removeClass().addClass('clock');
+  });
+
+  $('#left-button').on('click', 'button', function(event) {
+    let target = $('#ClockTower');
+      target.css('text-align', 'left');
+  });
+
+  $('#right-button').on('click', 'button', function(event) {
+    let target = $('#ClockTower');
+      target.css('text-align', 'right');
+  });
+
+  $('#center-button').on('click', 'button', function(event) {
+    let target = $('#ClockTower');
+      target.css('text-align', 'center');
+  });
+};  
+
 
 clock();
