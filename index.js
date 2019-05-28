dd5b72f8e99a96c561c2ac9c6508cdb28594aaa9
Stephanie Marchante <smarcha04@gmail.com>
2019-05-28 09:11:46 -0700

    implemented clock and css

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-const clock = function clock() {
+const clock = function clock(){
   let date = new Date();
-  let hour = date.getHours;
-  let minute = date.getMinutes;
-  let seconds = date.getSeconds;
+  let second = date.getSeconds();
+  let minute = date.getMinutes(); 
+  let hour = date.getHours(); 
+  let meridiem = "AM";
+  
+  if(hour == 0){
+    hour = 12;
+  }
+  
+  if(hour > 12){
+    hour = hour - 12;
+    meridiem = "PM";
+  }
+  
+  if (hour < 10 ) {
+    hour = '0' + hour;
+  }
 
-  let meridiem = 'AM';
+  if (minute < 10){
+    minute = '0' + minute;
+  }
+
+  if (second < 10){
+    second = '0' + second;
+  }
+
+  
+  let time = hour + ":" + minute + ":" + second + " " + meridiem;
+
+  document.getElementById("ClockTower").innerText = time;
+    
+  setTimeout(clock, 1000);
+  
 }
+
+clock();
